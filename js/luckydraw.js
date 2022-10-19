98f3b13f61183558b62d6a9b9b95136e9dcca33f
Neil <ws.niu@outlook.com>
2022-10-20 00:06:36 +0800

    fix

--- js/luckydraw.js
+++ js/luckydraw.js
@@ -1,7 +1,8 @@
 function drawNext() {
-  var lastdraw = $("#luckyperson").html();
-  var alllucky = $("#drawhistory").html();
-  // $("#drawhistory").html(lastdraw + '<br />' + alllucky);
-  var personname = data[Math.floor(Math.random() * data.length)];
-  $("#luckyperson").html(personname);
+    var lastdraw = $("#luckyperson").html();
+    var alllucky = $("#drawhistory").html();
+    // $("#drawhistory").html(lastdraw + '<br />' + alllucky);
+    var personname = data[Math.floor(Math.random() * data.length)];
+    var bodypart = body_data[Math.floor(Math.random() * body_data.length)];
+    $("#luckyperson").html(personname + " " + bodypart);
 }
