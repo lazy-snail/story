a1642ae6003f60366025cce90f5e6ecce2e5d374
Neil <ws.niu@outlook.com>
2022-10-20 23:46:19 +0800

    zxhdmx

--- js/luckydraw.js
+++ js/luckydraw.js
@@ -2,7 +2,9 @@ function drawNext() {
     var lastdraw = $("#luckyperson").html();
     var alllucky = $("#drawhistory").html();
     // $("#drawhistory").html(lastdraw + '<br />' + alllucky);
-    var personname = data[Math.floor(Math.random() * data.length)];
-    var bodypart = body_data[Math.floor(Math.random() * body_data.length)];
-    $("#luckyperson").html(personname + " " + bodypart);
+    // var personname = data[Math.floor(Math.random() * data.length)];
+    // var bodypart = body_data[Math.floor(Math.random() * body_data.length)];
+    // $("#luckyperson").html(personname + " " + bodypart);
+    var personname = t_f[Math.floor(Math.random() * t_f.length)];
+    $("#luckyperson").html(personname);
 }
